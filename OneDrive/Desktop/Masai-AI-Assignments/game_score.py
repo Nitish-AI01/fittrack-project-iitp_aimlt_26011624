68fb80e4ed70f8acd8ed8ebb0a701e70335e9d84
Nitish-AI01 <ram.reddy5162@gmail.com>
2026-02-12 11:28:17 +0530

    Player score calculator program

---
+++ OneDrive/Desktop/Masai-AI-Assignments/game_score.py
@@ -0,0 +1,10 @@
+name = input("Enter player name: ")
+games = int(input("Enter number of games played: "))
+score = int(input("Enter total score: "))
+
+average = score / games
+
+print("Player:", name)
+print("Games Played:", games)
+print("Total Score:", score)
+print("Average Score:", average)
